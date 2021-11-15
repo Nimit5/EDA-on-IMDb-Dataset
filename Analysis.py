ab54d0a95be007c8d1965ac47675c45c8d407d5f
Nimit Jain <55830302+Nimit5@users.noreply.github.com>
2021-11-15 21:03:44 +0530

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("Hello")
+x=[1,2,3,4,5]
+y=[10,20,30,20,10]
+# plotting the points
+plt.plot(x, y)
+ 
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+ 
+# giving a title to my graph
+plt.title('My first graph!')
+ 
+# function to show the plot
+plt.show()
